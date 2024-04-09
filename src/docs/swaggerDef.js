099b739711d3056f47a94e640ee9aa7fe285ae24
Jerald Jon Pablo <jeraldjonpablo@gmail.com>
2024-04-10 07:45:22 +0800

    fix: remove invalid package.json call

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,11 +1,9 @@
-import { version } from '../../package.json';
 import { port } from '../config/config.js';
 
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
     title: 'Hanpoom Shipments Demo API documentation',
-    version,
     license: {
       name: 'MIT',
       url: 'https://github.com/flamechaoz/shipments',
