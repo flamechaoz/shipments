81703aadcd59681abba51f503663c34f64834b41
Jerald Jon Pablo <jeraldjonpablo@gmail.com>
2024-04-10 13:24:41 +0800

    feat: tweak the response to fit expected output

--- src/services/picking-slip.service.js
+++ src/services/picking-slip.service.js
@@ -5,9 +5,56 @@ const getPickingSlips = async (options) => {
     const { limit, status } = options;
     const pickingSlips = await prisma.pickingSlip.findMany({
         take: limit,
+        select: {
+            order_id: true,
+            id: true,
+            picking_slip_dates: {
+              select: {
+                printed_at: true,
+                inspected_at: true,
+                shipped_at: true,
+                held_at: true,
+              }
+            },
+            picking_slip_items: {
+              select: {
+                is_pre_order: true,
+              }
+            }
+        }
+      
     });
+
+    // restructure result to match expected output
+    const pickingSlipsWithStatusAndPreOrderItem = pickingSlips.map(pickingSlip => {
+        
+        const printed = pickingSlip.pickingSlipDates?.printed_at !== null &&
+                        pickingSlip.pickingSlipDates?.inspected_at === null &&
+                        pickingSlip.pickingSlipDates?.shipped_at === null &&
+                        pickingSlip.pickingSlipDates?.held_at === null;
+        
+        const held = pickingSlip.pickingSlipDates?.held_at !== null;
+    
+        const hasPreOrderItem = pickingSlip.picking_slip_items.some(item => item.is_pre_order === 1);
+    
+        let pickingSlipStatus;
+        if (held) {
+          pickingSlipStatus = 'held';
+        } else if (printed) {
+          pickingSlipStatus = 'printed';
+        } else {
+          pickingSlipStatus = 'not printed';
+        }
+    
+        return {
+          order_id: pickingSlip.order_id,
+          picking_slip_id: pickingSlip.id,
+          picking_slip_status: pickingSlipStatus,
+          has_pre_order_item: hasPreOrderItem,
+        };
+      });
   
-    return pickingSlips;
+    return pickingSlipsWithStatusAndPreOrderItem;
 };
 
 export const pickingSlipService = {
